4f549754a49db8f33244e0ca8c326a2e48abcaf7
aled1027 <a.led1027@gmail.com>
2016-02-29 15:36:36 -0800

    basic hash table cache working

--- c_code/cache.c
+++ c_code/cache.c
@@ -1,48 +1,114 @@
 #include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "cache.h"
 
-//typedef uint64_t (*hash_func)(ckey_t key);
+uint64_t default_cache(ckey_t k)
+{
+    return *k % 100;
+}
 
-cache_t create_cache(uint64_t maxmem) {
-    cache_t c = malloc(maxmem);
+typedef struct
+{
+    cval_t val;
+    uint32_t size; // size of val in bytes
+} cache_entry;
+
+struct cache_obj 
+{
+    // http://stackoverflow.com/questions/6316987/should-struct-definitions-go-in-h-or-c-file
+    uint64_t size;
+    cache_entry *entries;
+    bool *is_used;
+    hash_func hash;
+
+};
+
+cache_t create_cache(uint64_t maxmem, hash_func h) 
+{
+    cache_t c = calloc(1, sizeof(struct cache_obj));
+    c->size = maxmem;
+    c->entries = calloc(maxmem, sizeof(cache_entry));
+    c->is_used = calloc(maxmem, sizeof(bool));
+
+    if (h == NULL) {
+        c->hash = default_cache;
+    } else {
+        c->hash = h;
+    }
+
     return c;
 }
 
-void cache_set(cache_t cache, ckey_t key, cval_t *val)
+void cache_set(cache_t cache, ckey_t key, cval_t val, uint32_t val_size)
 {
-    assert(cache && key && val);
-    return;
+    uint64_t hash = cache->hash(key);
+    assert(hash < cache->size && "won't be able to access that memory");
+
+    if (cache->is_used[hash]) {
+        cache_delete(cache, key);
+    }
+    cache->is_used[hash] = true;
+    cache_entry *e = &cache->entries[hash];
+
+
+    // do this kind of strange thing to retain constness of cval_t
+    void *memory = calloc(1, val_size);
+    memcpy(memory, val, val_size);
+
+    e->size = val_size;
+    e->val = memory;
 }
 
-cval_t cache_get(cache_t cache, ckey_t key)
+cval_t cache_get(cache_t cache, ckey_t key, uint32_t *val_size)
 {
-    assert(cache && key);
-    return 0;
+    void *ret;
+    uint64_t hash = cache->hash(key);
+    if (cache->is_used[hash]) {
+        cache->is_used[hash] = true;
+        cache_entry *e = &cache->entries[hash];
+        ret = calloc(1, e->size);
+        memcpy(ret, e->val, e->size);
+        *val_size = e->size;
+    } else {
+        ret = NULL;
+    }
+    return ret;
 }
 
 void cache_delete(cache_t cache, ckey_t key) 
 {
-    assert(cache && key);
-    return;
+    uint64_t hash = cache->hash(key);
+    cache->is_used[hash] = false;
+    free((void *) cache->entries[hash].val); // TODO don't need to do this - use realloc
 }
 
 uint64_t cache_space_used(cache_t cache)
 {
-    assert(cache);
-    return 0;
+    uint64_t mem = 0;
+    for (uint8_t i = 0; i < cache->size; ++i) {
+        uint64_t hash = cache->hash(&i);
+        if (cache->is_used[hash]) {
+            mem += cache->entries[hash].size;
+        }
+    }
+    return mem;
 }
 
 void destroy_cache(cache_t cache)
 {
-    assert(cache);
-    return;
-}
-
-void print_cache(cache_t cache) 
-{
-    assert(cache);
-    return;
-}
+    for (uint64_t i = 0; i < cache->size; i++) {
+        free((void *) cache->entries[i].val);
+        cache->entries[i].val = NULL;
+    }
+    free(cache->entries);
+    cache->entries = NULL;
+    free(cache->is_used);
+    cache->is_used = NULL;
+    free(cache);
+    cache = NULL;
+} 
--- c_code/main.c
+++ c_code/main.c
@@ -1,7 +1,91 @@
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "cache.h"
+
+
+void test_cache_space()
+{
+    // TEST doesn't account for evictions!
+    uint8_t k, v;
+    uint32_t m, mem = 0;
+    cache_t c = create_cache(100, NULL);
+    assert(cache_space_used(c) == mem);
+    uint32_t r = rand() % 15;
+    for (uint32_t i = 0; i < r; i++) {
+        k = rand() % 100;
+        v = rand() % 255;
+        m = rand() % 10;
+        mem += m;
+        cache_set(c, &k, &v, m);
+        uint32_t space = cache_space_used(c);
+        if (space != mem) {
+            printf("test_cache_space failed\n");
+            printf("i = %" PRIu32 "\n", i);
+            printf("m = %" PRIu32 "\n", m);
+            printf("mem = %" PRIu32 "\n", mem);
+            printf("r = %" PRIu32 "\n", r);
+        }
+    }
+}
+
+void test_delete_cache()
+{
+
+}
+
+void test_collision_cache()
+{
+    return;
+}
+
+void test_set_get()
+{
+    uint8_t *val, k, v;
+    uint32_t *size = malloc(sizeof(uint32_t));
+
+    cache_t c = create_cache(100, NULL);
+
+    k = 0;
+    v = 99;
+    cache_set(c, &k, &v, 1);
+
+    k = 89;
+    v = 23;
+    cache_set(c, &k, &v, 2);
+
+    k = 30;
+    v = 255;
+    cache_set(c, &k, &v, 1);
+
+    k = 89;
+    val = (uint8_t *) cache_get(c, &k, size);
+    assert(*val == 23);
+    free(val);
+
+    k = 30;
+    val = (uint8_t *) cache_get(c, &k, size);
+    assert(*val == 255);
+    free(val);
+
+    k = 0;
+    val = (uint8_t *) cache_get(c, &k, size);
+    assert(*val == 99);
+    free(val);
+
+    k = 10;
+    val = (uint8_t *) cache_get(c, &k, size);
+    assert(val == NULL);
+
+    free(size);
+    destroy_cache(c);
+}
+
 
 int main(int argc, char *argv[]) 
 {
     assert(argc && argv);
+    test_cache_space();
+    test_set_get();
     return 0;
 }
